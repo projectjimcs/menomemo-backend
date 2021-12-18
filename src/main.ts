2ba344111f9fc4b3ce101b64b94c64d877f8a64c
Jim <projectjimcs@gmail.com>
2021-12-17 18:27:26 -0800

    Logic to send cookie back to frontend completed

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: 'http://localhost:3000',
+      origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
       credentials: true,
     }
   });
